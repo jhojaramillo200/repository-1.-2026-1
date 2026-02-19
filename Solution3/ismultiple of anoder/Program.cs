a776e7ffc1aacd5df460ec9ff599b5b1ca2359bd
JHONATAN JARAMILLO GOMEZ <jhonatanjaramillo1158857@correo.itm.edu.co>
2026-02-18 19:08:20 -0500

    create blank solution

---
+++ Solution3/ismultiple of anoder/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ingrese un numero");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+if(numberInt % 2 == 0)
+{
+    Console.WriteLine(" el numero es par");
+}
+else
+{
+    Console.WriteLine(" el numero es impar");
+}
